d9becd3b25dc9a1e25026e32e5a338abfbee5faa
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2019-05-26 20:40:24 +0100

    Add test message generator

---
+++ integration_test/generate.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+)
+
+//go:generate protoc -I . test.proto --go_out=.
+func main() {
+	for i, val := range []string{
+		"ClientRequest",
+		"ServerResponse",
+		"ServerMessage1",
+		"ServerMessage2",
+	} {
+		marshalled, _ := proto.Marshal(&Outer{
+			OuterValue: &Inner{
+				InnerValue: val,
+				InnerNum:   int64(i + 1),
+			},
+			OuterNum: int64(i + 1),
+		})
+
+		fmt.Println(i, val, base64.StdEncoding.EncodeToString(marshalled))
+	}
+}

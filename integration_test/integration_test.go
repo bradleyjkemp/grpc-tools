84e839c1b81bdd4a6d65aef97c31097a36a835b6
Bradley Kemp <bradleyjkemp@gmx.co.uk>
2019-07-08 22:09:08 +0100

    Add grpc-web/curl tests

--- integration_test/integration_test.go
+++ integration_test/integration_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/bradleyjkemp/grpc-tools/grpc-replay/replay"
 	"github.com/bradleyjkemp/grpc-tools/internal/proxydialer"
 	"net/url"
+	"os/exec"
 	"testing"
 )
 
@@ -67,4 +68,20 @@ func TestIntegration(t *testing.T) {
 	if replayErr != nil {
 		t.Fatal("Unexpected error:", replayErr)
 	}
+
+	cmd := curlCommand("http://grpc-web.github.io/grpc.gateway.testing.EchoService/Echo")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Fatal("Unexpected error:", err, string(out))
+	}
+
+	cmd = curlCommand("https://grpc-web.github.io:1234/grpc.gateway.testing.EchoService/Echo")
+	if out, err := cmd.CombinedOutput(); err != nil {
+		t.Fatal("Unexpected error:", err, string(out))
+	}
+}
+
+func curlCommand(url string) *exec.Cmd {
+	cmd := exec.Command("curl", "-X", "POST", url, "-H", "Pragma: no-cache", "-H", "X-User-Agent: grpc-web-javascript/0.1", "-H", "Origin: http://localhost:8081", "-H", "Accept-Encoding: gzip, deflate, br", "-H", "Accept-Language: en-US,en;q=0.9", "-H", "custom-header-1: value1", "-H", "User-Agent: Mozilla/5.0", "-H", "Content-Type: application/grpc-web+proto", "-H", "Accept: */*", "-H", "X-Grpc-Web: 1", "-H", "Cache-Control: no-cache", "-H", "Referer: http://localhost:8081/echotest.html", "-H", "Connection: keep-alive")
+	cmd.Env = []string{fmt.Sprintf("all_proxy=http://localhost:%d", dumpPort)}
+	return cmd
 }
